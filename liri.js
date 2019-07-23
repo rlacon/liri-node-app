a3492ec5942d60b3f8ab53abf1e1c7dc66e4641f
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-23 17:08:38 -0400

    Added switch cases to search for movie and song data

--- liri.js
+++ liri.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 // fs.readFile("random.txt", "utf8", function (error, data) {
 // })
 var fs = require("fs");
@@ -7,31 +6,34 @@ var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var axios = require("axios");
 var spotify = new Spotify(keys.spotify);
+var song = process.argv[2];
 var operand = process.argv[2];
 
 // OMDB is working -----------------------------------------------------------------
-var movieName = process.argv[2];
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+var getMeMovie = function (movieName) {
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-console.log(queryUrl);
+    console.log(queryUrl);
 
-axios.get(queryUrl).then(
-    function (response) {
-        console.log(response);
-        if (response) {
-            console.log("Title: " + response.data.Title);
-            console.log("Year: " + response.data.Year);
-            console.log("IMDB Rating: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-            console.log("Country: " + response.data.Country);
-            console.log("Language: " + response.data.Language);
-            console.log("Plot: " + response.data.Plot);
-            console.log("Actors: " + response.data.Actors);
-        }
-    });
-var song = process.argv[2];
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(response);
+            if (response) {
+                console.log("Title: " + response.data.Title);
+                console.log("Year: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+            }
+        });
+}
 
-// Spotify is working
+// Spotify is working -----------------------------------------------------------------
+
+var getMeSpotify = function(song) {
 spotify
     .search({ type: 'track', query: song, limit: 1 })
     .then(function (response) {
@@ -48,3 +50,32 @@ spotify
     .catch(function (err) {
         console.log(err);
     });
+}
+
+var pick = function (caseData, functionData) {
+    switch (caseData) {
+        case "concert-this":
+            getMyBands(functionData);
+            break;
+        case "spotify-this-song":
+            getMeSpotify(functionData);
+            break;
+        case "movie-this":
+            getMeMovie(functionData);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+            console.log("LIRI doesn't know that");
+    }
+};
+
+// Function which takes in command line arguments and executes correct function accordingly
+var runThis = function (argOne, argTwo) {
+    pick(argOne, argTwo);
+};
+
+// MAIN PROCESS
+// =====================================
+runThis(process.argv[2], process.argv.slice(3).join(" "));
